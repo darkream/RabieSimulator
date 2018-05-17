be8a188f433a587d2fbba696172072ad4e707630
Fune <dark_ream@hotmail.com>
2018-05-17 19:53:20 +0900

    Initial Setting
    only empty project

---
+++ Assets/Script/Initial_dump_for _delete.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Initial_dump_for_delete : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
