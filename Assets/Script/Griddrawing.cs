6bfbfbe6e178d5211287c2691c4c876a909302d7
Fune <dark_ream@hotmail.com>
2018-05-26 00:11:59 +0900

    Grid draw add
    Can now drawing grid
    (Use griddraw script)

---
+++ Assets/Script/Griddrawing.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Griddrawing : MonoBehaviour {
+
+	public GameObject horiobj;
+	public GameObject vertiobj;
+	private LineRenderer horizonline;
+	private LineRenderer verticalline;
+	private const int maxgridnum = 32;
+	public int horizongridnum =1 ;
+	public int verticalgridnum =1 ;
+	//will need to get map size from object ,but use this for temporary
+	private const double mapsize = 32;
+	// Use this for initialization
+
+	void Start () {
+		horizonline = horiobj.GetComponent<LineRenderer>();
+		verticalline =  vertiobj.GetComponent<LineRenderer>();
+		int horigriduse =(horizongridnum*2)-1;
+		int vertigriduse =(verticalgridnum*2)-1;
+		 // Set some positions
+        Vector3[] horipositions = new Vector3[horigriduse];
+		Vector3[] vertipositions = new Vector3[vertigriduse];
+
+
+		//draw like this
+		//	_ _ _ _ _					 			_ _ _ _ _
+		// |		 |   			   |   		   |	|	 |
+		// |		 | boarder +  	   |        =  |	|	 |
+		// |		 |			       |   		   |	|	 |
+		// |_ _ _ _ _|				_ _|   		   |_ _ | _ _|
+
+
+		//horizontal divide
+		double HorizontalGridSize= mapsize / (float)horizongridnum ; //gridnumber
+		//vertical divide
+		double VerticalGridSize= mapsize / (float)verticalgridnum ; //gridnumber
+		int i;
+		//horizontal divider draw 
+		horipositions[0] = new Vector3(0.0f, 0.0f, 0.0f);
+		
+		for( i = 1;i<horigriduse;i++)
+		{
+ 			//positions[0] is 0 0 0
+			if(i % 2 != 0) //  draw _ line  left to right
+			{
+			 horipositions[i] = new Vector3 (horipositions[i-1].x+(float)HorizontalGridSize , 0.0f ,horipositions[i-1].z   );
+			}
+			 if(i % 2 == 0) //  draw | line 
+			{
+					 if((i/2) % 2 != 0) // down to top
+					{
+						horipositions[i] = new Vector3 (horipositions[i-1].x , 0.0f,horipositions[i-1].z + (float)mapsize ); //should be hiegh map size
+					}
+					 else if((i/2) % 2 == 0)// top to down
+					 {
+						horipositions[i] = new Vector3 (horipositions[i-1].x , 0.0f,horipositions[i-1].z -(float)mapsize );//should be width map size
+					 }
+			}	
+		}
+
+
+		//vertical divider draw 
+		vertipositions[0] = new Vector3(0.0f, 0.0f, 0.0f);
+
+		for( i = 1;i<vertigriduse;i++)
+		{
+ 			//positions[0] is 0 0 0
+			if(i % 2 != 0) //  draw | line down to top
+			{
+			 vertipositions[i] = new Vector3 (vertipositions[i-1].x , 0.0f ,vertipositions[i-1].z+(float)VerticalGridSize   );
+			}
+			 if(i % 2 == 0) //  draw _ line 
+			{
+					 if((i/2) % 2 != 0) // left to right
+					{
+						vertipositions[i] = new Vector3 (vertipositions[i-1].x + (float)mapsize, 0.0f,vertipositions[i-1].z  ); //should be hiegh map size
+					}
+					 else if((i/2) % 2 == 0)// right to left
+					 {
+						vertipositions[i] = new Vector3 (vertipositions[i-1].x- (float)mapsize , 0.0f,vertipositions[i-1].z  );//should be width map size
+					 }
+			}	
+		}
+
+
+      
+
+
+        horizonline.positionCount = horipositions.Length;
+		verticalline.positionCount = vertipositions.Length;
+		horizonline.SetPositions(horipositions);
+		verticalline.SetPositions(vertipositions);
+       
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
